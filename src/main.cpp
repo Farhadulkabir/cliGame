ddf150c6d5da9b4a6927282dc2c6eed8701b2827
WAD <farhadulkabirr@gmail.com>
2025-06-06 10:58:27 +0600

    improving the screen clearing

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,24 @@
 #include "array.h"
 #include "getch.h"
-#include <cstdlib>
+#include <iostream>
 int main() {
   int p{x / 2}, q{y / 2};
   char arr[x][y];
   while (true) {
     arr[p][q] = '@';
+    std::cout << "\033[2J\033[H";
     arrayPrint(arr, x, y);
     arrayClear(arr, x, y);
     char c = getch();
     if (c == 'q') {
       break;
     } else if (c == 'w' && p > 0) {
-      system("clear");
       p--;
     } else if (c == 's' && p < x - 1) {
-      system("clear");
       p++;
     } else if (c == 'a' && q > 0) {
-      system("clear");
       q--;
     } else if (c == 'd' && q < y - 1) {
-      system("clear");
       q++;
     }
   }
